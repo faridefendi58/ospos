8db29cb3e41b7afbb77bd917bfee9aef04dd590e
Farid Efendi <miftahfaridse@gmail.com>
2018-09-03 15:38:20 +0700

    Double robot blocking

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -4,6 +4,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<base href="<?php echo base_url();?>" />
 	<title><?php echo $this->config->item('company') . ' | ' . $this->lang->line('common_powered_by') . ' OSPOS ' . $this->config->item('application_version') ?></title>
+    <meta name="robots" content="noindex">
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="<?php echo 'dist/bootswatch/' . (empty($this->config->item('theme')) ? 'flatly' : $this->config->item('theme')) . '/bootstrap.min.css' ?>"/>
 
